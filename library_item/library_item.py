f4afe2989af3db7c143a31fc0a898a5466c5f1f9
Ralph.Vitug <rralph@rrc.ca>
2025-09-05 09:35:21 -0500

    executed diagram with dunder method and exceptions that prints messages when error arises

--- library_item/library_item.py
+++ library_item/library_item.py
@@ -6,6 +6,7 @@ Description: A class to manage LibraryItem objects.
 __author__ = "Ralph Vitug"
 __version__ = "Act.1.0.0"
 
+from genre.genre import Genre
 
 class LibraryItem:
     """
@@ -19,4 +20,25 @@ class LibraryItem:
 
     """
 
-    
+    def __init__(self, title: str, author: str, genre: Genre):
+
+        if len(title.strip())> 0: # strip any white space from title
+            self.__title = title # name mangling
+        else:
+            raise ValueError("Title cannot be blank") # prints a message if title is blank
+        
+        if len(author.strip())> 0: # strips any white space from author
+            self.__author = author # name mangling
+        else:
+            raise ValueError("Author cannot be blank") # prints a message if author is blank
+        
+        if isinstance(genre, Genre): # checks if genre is valid
+            self.__genre = genre
+        else:
+            raise ValueError("Invalid Genre") # prints a message if genre is invalid
+        
+
+        
+
+        
+
