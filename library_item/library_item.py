334766b9981a42b13973c0cb072fc456ca54f379
Ralph.Vitug <rralph@rrc.ca>
2025-09-05 09:20:15 -0500

    Initialized class for LibraryItem with args

--- library_item/library_item.py
+++ library_item/library_item.py
@@ -1,6 +1,22 @@
 """"
 Description: A class to manage LibraryItem objects.
 """
-__author__ = ""
-__version__ = ""
 
+
+__author__ = "Ralph Vitug"
+__version__ = "Act.1.0.0"
+
+
+class LibraryItem:
+    """
+    Contains items from library
+
+    args:
+
+        title (str): name of items
+        author (str): name of writer or author
+        genre (Genre): type of the item that it belongs to
+
+    """
+
+    
