6ee6de341ad124952b83e34a85282671341cebe3
Ralph.Vitug <rralph@rrc.ca>
2025-09-08 12:48:35 -0500

    using init to declare class attribute based on the class diagram provided

---
+++ client/client.py
@@ -0,0 +1,42 @@
+__author__ = "Ralph Vitug"
+__version__ = "ISD 2.0.0"
+
+class Client: # initialing a class Client
+    """
+    
+    Contains client information
+
+    args:
+
+        client_number (int): An integer value representing the client number
+        first_name (str): A string value the client's first name
+        last_name (str): A string value the client's last name
+        email_address (str): A string value the client's email address
+
+
+    """
+
+    def __init__(self, client_number: int, first_name: str, last_name: str, email_address: str):
+        # using init to declare class attribute based on the class diagram provided
+
+        if isinstance(client_number, int): # checks if client number is valid
+            self.__client_number = client_number
+        else:
+            raise ValueError("Client number must be an integer/numeric type")
+            # raises value error if client is not valid
+        
+        if len(first_name.strip())> 0: # strips any white space from first name
+            self.__first_name = first_name
+        else:
+            raise ValueError("First name cannot be blank")
+            # raises value error if first name is blank
+        
+        if len(last_name.strip())> 0: # strips any white space from last name
+            self.__last_name = last_name
+        else:
+            raise ValueError("Last name cannot be blank")
+            # raises value error if last name is blank
+        
+        if len(email_address.strip())> 0: # checks the validity of email address
+            self.__email_address = email_address
+        
