5a35be09bf0a0f89d89d7a72738136dd56aa1da7
Ralph.Vitug <rralph@rrc.ca>
2025-10-01 12:34:06 -0500

    updated savings_account

--- bank_account/savings_account.py
+++ bank_account/savings_account.py
@@ -21,20 +21,49 @@ class SavingsAccount(BankAccount):
         be before further service charges are applied.
 
     raises/exception:
-
-
+        minimum_balance (float): converts to float, but
+        defaults to 50.00 if value is invalid.
     """
     SERVICE_CHARGE_PREMIUM = 2.00
 
     def __init__(self, account_number: int, client_number: int,
                  balance: float, date_created: date,
                  minimum_balance: float):
-        super().__init__(account_number, client_number, balance,
-                         date_created, minimum_balance)
+        super().__init__(account_number, client_number, 
+                        balance,date_created)
         
         try:
             self.__minimum_balance = float(minimum_balance)
 
         except ValueError:
-            self.__minimum_balance = 50
+            self.__minimum_balance = 50.00
+
+    def get_service_charges(self) -> float:
+        """
+        Calculates service charges
+
+        Returns:
+            balance >= minimum_balance + BSC.
+            balance < minimum_balance + (BSC * SCP)
+        """
+
+        if self.balance >= self.__minimum_balance:
+            return BankAccount.BASE_SERVICE_CHARGE
+        else:
+            return BankAccount.BASE_SERVICE_CHARGE * SavingsAccount.SERVICE_CHARGE_PREMIUM
+
+    def __str__(self) -> str:
+        """
+        String method of SavingsAccount
+
+        returns: 
+            str: details as required
+        """
+
+        return (
+            f"Account Number: {self.account_number} Balance: ${self.balance:,.2f}\n"
+            f"Minimum Balance: ${self.__minimum_balance:,.2f} "
+            f"Account Type: Savings"
+        )
+
 
