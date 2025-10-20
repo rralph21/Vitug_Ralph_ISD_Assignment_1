5a6a7e5dfe10b4bb9a3ed8897e9d61f43e634027
Ralph.Vitug <rralph@rrc.ca>
2025-10-20 12:24:36 -0500

    feat: service_charge, minimum_balance, and overdraft strategies

---
+++ patterns/strategy/overdraft.py
@@ -0,0 +1,44 @@
+__author__ = "Ralph Vitug"
+__version__ = "1.0.0"
+
+from typing import List
+from patterns.strategy.service_charge import ServiceChargeStrategy
+from bank_account.bank_account import BankAccount
+
+class OverdraftStrategy(ServiceChargeStrategy):
+    """
+    OverdraftStrategy charges accounts that exceeds the maximum
+    threshold of balance insuffeciency.
+    """
+
+    def __init__(self, overdraft_limit: float, overdraft_rate: float):
+        
+        try:
+            self.__overdraft_limit = float(overdraft_limit)
+        
+        except ValueError:
+            self.__overdraft_limit = -100.0
+
+        try:
+            self.__overdraft_rate = float(overdraft_rate)
+
+        except ValueError:
+            self.__overdraft_rate = 0.05
+        
+    def calculate_service_charges(self, account: BankAccount) -> float:
+
+        """
+        Method to calculate service charges.
+
+        args:
+            get_service_charges (float): charges that returns float
+
+        """
+
+        if self.balance < self.__overdraft_limit:
+        
+            overdraft_cost = abs(self.balance - self.__overdraft_limit) * self.__overdraft_rate
+            return BankAccount.BASE_SERVICE_CHARGE + overdraft_cost
+        else:
+            
+            return BankAccount.BASE_SERVICE_CHARGE
---
+++ patterns/strategy/service_charge.py
@@ -0,0 +1,23 @@
+__author__ = "Ralph Vitug"
+__version__ = "1.0.0"
+
+from abc import ABC, abstractmethod
+from bank_account import BankAccount
+from typing import List
+
+class ServiceChargeStrategy(ABC):
+    """
+    ServiceChargeStrategy charges a fee to appropriate back account
+    const: BASE_SERVICE_SERVICE: float = 0.50
+    """
+
+    BASE_SERVICE_CHARGE: float = 0.50
+
+    @abstractmethod
+    def calculate_service_charges(self, account: BankAccount): float
+    """
+    calculate_service_charge method will be implemented in each
+    of the ssublcasses of ServiceChargeStrategy baed on the specific
+    strategy being employed.
+    """
+    pass
