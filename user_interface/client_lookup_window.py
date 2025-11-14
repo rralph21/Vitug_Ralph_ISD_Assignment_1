7c68a4a9bae043279626291dcf9188489c90025c
Ralph.Vitug <rralph@rrc.ca>
2025-11-13 20:45:59 -0600

    add: client account

--- user_interface/client_lookup_window.py
+++ user_interface/client_lookup_window.py
@@ -23,12 +23,12 @@ class ClientLookupWindow(LookupWindow):
     self.client_number_edit.textChange.connect(self.on_text_changed)
     self.account_table.cellClicked.connect(self.on_select_account)
 
-    @Slot
+    @Slot()
     def on_lookup_client(self) -> None:
         """
         slot for lookup_button signal
 
-        raises: ValueError
+        ValueError:
             QMessageBox.Warning: error message
         """
 
@@ -40,7 +40,7 @@ class ClientLookupWindow(LookupWindow):
         except ValueError:
             QMessageBox.warning(
                 self,
-                "Non-Numberic Client",
+                "Non-Numeric Client",
                 "Client Number must be a numeric value."
             )
 
@@ -52,3 +52,45 @@ class ClientLookupWindow(LookupWindow):
         self.reset_display()
 
         if client_number not in self.client_listing:
+            QMessageBox.information(
+                self,
+                "Client Not Found",
+                f"Client Number {client_number} not found"
+            )
+
+            #reset
+            self.reset_display()
+            return
+        
+        #get Client object if calient is valid
+        client = self.client_listing[client_number]
+
+        #display client information
+        self.client_info_label.setText(str(client))
+
+        #display all client's account
+        self.account_table.setRowCount(0)
+
+        for account in self.accounts.values():
+            if account.client_number == client.client_number:
+
+                row = self.account_table.rowCount()
+                self.account_table.insertRow(row)
+
+                account_number_item = QTableWidgetItem(str(account.account_number))
+
+                balance_text = f"${account.balance:,.2f}"
+                balance_item = QTableWidgetItem(balance_text)
+
+                data_text = str(account.date_created.date())
+                date_item = QTableWidgetItem(data_text)
+
+                account_type_text = account.__class__.__name__
+                account_type_item = QTableWidgetItem(account_type_text)
+
+                self.account_table.setItem(row, 0, account_number_item)
+                self.account_table.setItem(row, 1, balance_item)
+                self.account_table.setItem(row, 2, date_item)
+                self.account_table.setItem(row, 3, account_type_item)
+
+        self.account_table.resizeColumnsToContents()
