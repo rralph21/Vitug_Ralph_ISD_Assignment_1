6936dd9b2c7d52eab3a710901fe13af4bb46fddc
Ralph.Vitug <rralph@rrc.ca>
2025-11-13 20:33:25 -0600

    client_lookup continuation

--- user_interface/client_lookup_window.py
+++ user_interface/client_lookup_window.py
@@ -10,6 +10,7 @@ from user_interface.account_details_window import AccountDetailsWindow
 from user_interface.manage_data import load_data
 from user_interface.manage_data import update_data
 from bank_account.bank_account import BankAccount
+from .manage_data import load_data
 
 class ClientLookupWindow(LookupWindow):
     
@@ -21,3 +22,33 @@ class ClientLookupWindow(LookupWindow):
     self.lookup_button.clicked.connect(self.on_lookup_client)
     self.client_number_edit.textChange.connect(self.on_text_changed)
     self.account_table.cellClicked.connect(self.on_select_account)
+
+    @Slot
+    def on_lookup_client(self) -> None:
+        """
+        slot for lookup_button signal
+
+        raises: ValueError
+            QMessageBox.Warning: error message
+        """
+
+        client_text = self.client_number_edit.text().strip()
+
+        try:
+            client_number = int(client_text)
+
+        except ValueError:
+            QMessageBox.warning(
+                self,
+                "Non-Numberic Client",
+                "Client Number must be a numeric value."
+            )
+
+            #reset
+            self.reset_display()
+            return
+        
+        #reset before showing new results
+        self.reset_display()
+
+        if client_number not in self.client_listing:
