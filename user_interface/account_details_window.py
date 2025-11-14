67072ce9c526d77d4fcbc1127fe34c49152fdc36
Ralph.Vitug <rralph@rrc.ca>
2025-11-13 21:51:23 -0600

    add: account validation, on_apply_transaction and on_exit

--- user_interface/account_details_window.py
+++ user_interface/account_details_window.py
@@ -7,6 +7,7 @@ from PySide6.QtWidgets import QMessageBox
 from PySide6.QtCore import Signal
 from bank_account.bank_account import BankAccount
 import copy
+from PySide6.QtCore import Slot
 
 class AccountDetailsWindow(DetailsWindow):
     """
@@ -21,3 +22,31 @@ class AccountDetailsWindow(DetailsWindow):
             None
         """
         super().__init__()
+
+        if isinstance(account, BankAccount):
+
+            self.account = copy.copy(account)
+
+            self.account_number_label.setText(str(self.account.account_number))
+
+            self.balance_label.setText(f"${self.account.balance:,.2f}")
+
+            #Connect buttons to their slots
+            self.deposit_button.clicked.connect(self.on_apply_transaction)
+            self.withdraw_button.clicked.connect(self.on_apply_transaction)
+            self.exit_button.clicked.connect(self.on_exit)
+
+    @Slot()
+    def on_apply_transaction(self):
+        """
+        Transaction handler
+        """
+        pass
+
+    @Slot()
+    def on_exit(self):
+        """
+        Exit handler
+        """
+        pass
+    
